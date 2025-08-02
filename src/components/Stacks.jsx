a26f925bb2d19655570ab6a4fc35b18acbe6c3fb
Fortexfreddie <106601096+Fortexfreddie@users.noreply.github.com>
2025-08-02 16:13:12 +0100

    Updated my stacks

--- src/components/Stacks.jsx
+++ src/components/Stacks.jsx
@@ -1,6 +1,26 @@
-import { SiMongodb, SiTailwindcss, SiDart, SiPhp, SiJavascript, SiGithub, SiGit, SiReact, SiFlutter, SiMysql, SiLaravel, SiNodedotjs, SiExpress, SiTypescript, SiCss3, SiNextdotjs, SiVercel, SiPostman, SiNetlify, SiDocker } from 'react-icons/si';
+import {
+  SiMongodb,
+  SiTailwindcss,
+  SiDart,
+  SiPhp,
+  SiJavascript,
+  SiGithub,
+  SiGit,
+  SiReact,
+  SiFlutter,
+  SiMysql,
+  SiLaravel,
+  SiNodedotjs,
+  SiExpress,
+  SiTypescript,
+  SiCss3,
+  SiVercel,
+  SiPostman,
+  SiNetlify,
+  SiDocker,
+} from "react-icons/si";
 import { motion } from "framer-motion";
-import useIsMobile from '../hooks/useIsMobile';
+import useIsMobile from "../hooks/useIsMobile";
 
 const techStacks = [
   {
@@ -34,7 +54,7 @@ const techStacks = [
       {
         name: "TypeScript",
         icon: <SiTypescript className="text-blue-400" size={36} />,
-        percentage: 70,
+        percentage: 75,
       },
     ],
   },
@@ -79,32 +99,32 @@ const techStacks = [
       {
         name: "Git",
         icon: <SiGit className="text-orange-500" size={36} />,
-        percentage: 50,
+        percentage: 80,
       },
       {
         name: "GitHub",
         icon: <SiGithub className="text-gray-300" size={36} />,
-        percentage: 70,
+        percentage: 85,
       },
       {
         name: "Vercel",
         icon: <SiVercel className="text-gray-300" size={36} />,
-        percentage: 70,
+        percentage: 85,
       },
       {
         name: "Netlify",
         icon: <SiNetlify className="text-teal-400" size={36} />,
-        percentage: 70,
+        percentage: 85,
       },
       {
         name: "Postman",
         icon: <SiPostman className="text-orange-500" size={36} />,
-        percentage: 65,
+        percentage: 85,
       },
       {
         name: "Docker",
         icon: <SiDocker className="text-sky-500" size={36} />,
-        percentage: 50,
+        percentage: 60,
       },
     ],
   },
@@ -117,9 +137,9 @@ const techStacks = [
         percentage: 50,
       },
       {
-        name: "Next.js",
-        icon: <SiNextdotjs className="text-black" size={36} />,
-        percentage: 20,
+        name: "Express",
+        icon: <SiExpress className="text-gray-300" size={36} />,
+        percentage: 10,
       },
       {
         name: "Laravel",
@@ -134,7 +154,7 @@ const techStacks = [
       {
         name: "Docker",
         icon: <SiDocker className="text-sky-500" size={36} />,
-        percentage: 50,
+        percentage: 60,
       },
       {
         name: "MongoDB",
@@ -146,59 +166,74 @@ const techStacks = [
 ];
 
 const Stack = () => {
-    const isMobile = useIsMobile();
-    return (  
-        <section id="Stacks" className="bg-gray-50 dark:bg-gray-900 pt-20 pb-10 w-full transition-colors duration-300">
-            <div className="lg:container mx-auto px-6 md:px-8 lg:px-12">
-                <h1 className="text-center text-3xl md:text-4xl text-gray-950 dark:text-gray-50 transition-colors duration-300 font-bold tracking-wider">My Tech Stack</h1>
-                <div className="mt-4 h-0.5 w-32 mx-auto bg-gradient-to-r from-gray-100 via-gray-400 to-gray-100 dark:from-gray-900 dark:via-gray-400 dark:to-gray-900 transition-colors duration-300"></div>
-                <p className="mt-6 text-lg text-gray-600 dark:text-gray-300 text-center transition-colors duration-300">“Technologies I Use to Build Modern Applications”</p>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
-                    {
-                        techStacks.map((techStack, index) => (
-                            <motion.div
-                                key={index}
-                                initial={{ opacity: 0, y: 100 }}
-                                whileInView={{ opacity: 1, y: 0 }} 
-                                transition={{ 
-                                    duration: 0.8,
-                                    delay: index * (isMobile ? 0.1 : 0.2), // Stagger based on position and screen size
-                                    ease: [0.25, 0.8, 0.25, 1], // cubic-bezier curve
-                                }}
-                                viewport={{ once: true }}
-                                className="bg-gray-200 dark:bg-gray-800 rounded-xl p-6 border border-gray-300 dark:border-gray-600 backdrop-blur-md transition-colors duration-300">
-                                <p className="text-lg font-semibold text-gray-600 dark:text-gray-50 transition-colors duration-300 mb-4">{techStack.category}</p>
-                                <div className="grid grid-cols-2 xl:grid-cols-3 gap-4">
-                                    {
-                                        techStack.items.map((item, i) => (
-                                            <motion.div
-                                                whileHover={{ scale: 1.03 }}
-                                                whileTap={{ scale: 0.9 }}
-                                                transition={{duration: 0.2}}
-                                                key={i} 
-                                                className="flex flex-col items-center p-4 bg-white dark:bg-gray-700 rounded-lg border border-gray-200 dark:border-gray-600 backdrop-blur-md hover:shadow-md transition-colors duration-500">
-                                                <span className="mb-2">
-                                                    {item.icon}
-                                                </span>
-                                                <span className="text-md font-medium text-gray-700 dark:text-gray-200 transition-colors duration-300">{item.name}</span>
-                                                <div className="flex flex-col justify-end h-full w-full mt-3">
-                                                    <span className='flex justify-between items-center w-full'>
-                                                        <span className="text-xs font-medium text-gray-600 dark:text-gray-300 transition-colors duration-300">{item.name}</span>
-                                                        <span className="text-xs font-medium text-gray-600 dark:text-gray-300 transition-colors duration-300">{item.percentage}%</span>
-                                                    </span>
-                                                    <progress value={item.percentage} max="100" className="mt-1 w-full h-2 rounded [&::-webkit-progress-bar]:bg-gray-200 dark:[&::-webkit-progress-bar]:bg-gray-400 [&::-webkit-progress-bar]:rounded [&::-webkit-progress-value]:bg-emerald-600 [&::-webkit-progress-value]:rounded transition-all duration-300"></progress>
-                                                </div>
-                                            </motion.div>
-                                        ))
-                                    }
-                                </div>
-                            </motion.div>
-                        ))
-                    }
-                </div>
-            </div>
-        </section>
-    );
-}
- 
+  const isMobile = useIsMobile();
+  return (
+    <section
+      id="Stacks"
+      className="bg-gray-50 dark:bg-gray-900 pt-20 pb-10 w-full transition-colors duration-300"
+    >
+      <div className="lg:container mx-auto px-6 md:px-8 lg:px-12">
+        <h1 className="text-center text-3xl md:text-4xl text-gray-950 dark:text-gray-50 transition-colors duration-300 font-bold tracking-wider">
+          My Tech Stack
+        </h1>
+        <div className="mt-4 h-0.5 w-32 mx-auto bg-gradient-to-r from-gray-100 via-gray-400 to-gray-100 dark:from-gray-900 dark:via-gray-400 dark:to-gray-900 transition-colors duration-300"></div>
+        <p className="mt-6 text-lg text-gray-600 dark:text-gray-300 text-center transition-colors duration-300">
+          “Technologies I Use to Build Modern Applications”
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
+          {techStacks.map((techStack, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: 100 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 0.8,
+                delay: index * (isMobile ? 0.1 : 0.2), // Stagger based on position and screen size
+                ease: [0.25, 0.8, 0.25, 1], // cubic-bezier curve
+              }}
+              viewport={{ once: true }}
+              className="bg-gray-200 dark:bg-gray-800 rounded-xl p-6 border border-gray-300 dark:border-gray-600 backdrop-blur-md transition-colors duration-300"
+            >
+              <p className="text-lg font-semibold text-gray-600 dark:text-gray-50 transition-colors duration-300 mb-4">
+                {techStack.category}
+              </p>
+              <div className="grid grid-cols-2 xl:grid-cols-3 gap-4">
+                {techStack.items.map((item, i) => (
+                  <motion.div
+                    whileHover={{ scale: 1.03 }}
+                    whileTap={{ scale: 0.9 }}
+                    transition={{ duration: 0.2 }}
+                    key={i}
+                    className="flex flex-col items-center p-4 bg-white dark:bg-gray-700 rounded-lg border border-gray-200 dark:border-gray-600 backdrop-blur-md hover:shadow-md transition-colors duration-500"
+                  >
+                    <span className="mb-2">{item.icon}</span>
+                    <span className="text-md font-medium text-gray-700 dark:text-gray-200 transition-colors duration-300">
+                      {item.name}
+                    </span>
+                    <div className="flex flex-col justify-end h-full w-full mt-3">
+                      <span className="flex justify-between items-center w-full">
+                        <span className="text-xs font-medium text-gray-600 dark:text-gray-300 transition-colors duration-300">
+                          {item.name}
+                        </span>
+                        <span className="text-xs font-medium text-gray-600 dark:text-gray-300 transition-colors duration-300">
+                          {item.percentage}%
+                        </span>
+                      </span>
+                      <progress
+                        value={item.percentage}
+                        max="100"
+                        className="mt-1 w-full h-2 rounded [&::-webkit-progress-bar]:bg-gray-200 dark:[&::-webkit-progress-bar]:bg-gray-400 [&::-webkit-progress-bar]:rounded [&::-webkit-progress-value]:bg-emerald-600 [&::-webkit-progress-value]:rounded transition-all duration-300"
+                      ></progress>
+                    </div>
+                  </motion.div>
+                ))}
+              </div>
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default Stack;
